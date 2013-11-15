eba7c76195bf3be8909698a4bd0d27fac6d78e38
Matthew Cairns <mattrcairns@gmail.com>
2013-11-15 19:11:10 +0000

    Added bullets

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
 import pygame
 from pygame.locals import *
 import sys
+import ship
 
 pygame.init()
 
 #Set screen resolution
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((480, 800))
+pygame.display.flip()
+#Initialize clock to control framerate
+clock = pygame.time.Clock()
+
+player_ship = ship.Ship(240, 600)
+
+bullets = []
+
+def main():
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    sys.exit()
+                if event.key == K_SPACE:
+                    print 'SHOOT'
+                    bullets.append(ship.Bullet(player_ship.x, player_ship.y, screen))
+        screen.fill((0,0,0))
+        player_ship.controls(screen)
+        player_ship.draw(screen)
+
+        for i in range(len(bullets)):
+            bullets[i].update()
+            bullets[i].draw(screen)
+
+        pygame.display.update()
 
 
-#Load the player assets
-ship = pygame.image.load('Assets/spaceship.png').convert()
+
+main()
 
 
-while True:
-    for event in pygame.event.get():
-        if event.type in (QUIT, KEYDOWN):
-            sys.exit()
-
-    pygame.display.update()
-    screen.blit(ship, (320, 240))
--- ship.py
+++ ship.py
@@ -15,9 +15,26 @@ class Ship(pygame.sprite.Sprite):
     def draw(self, screen):
         screen.blit(self.ship, (self.x, self.y))
 
-    def controls(self):
+    def controls(self, screen):
         keys = pygame.key.get_pressed()
         if keys[K_LEFT]:
             self.x -= 5
         if keys[K_RIGHT]:
             self.x += 5
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, screen):
+        pygame.sprite.Sprite.__init__(self)
+        self.bullet = pygame.image.load('Assets/bullet.png').convert()
+        print 'BULLET'
+        self.x = x
+        self.y = y
+
+    def draw(self, screen):
+        screen.blit(self.bullet, (self.x, self.y))
+
+    def update(self):
+        self.y -= 10
+
+
