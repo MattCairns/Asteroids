6bd6a0e53da3de60817266dcb251a8f141632394
Matthew Cairns <mattrcairns@gmail.com>
2013-11-15 18:29:27 +0000

    Added left and right controls for ship.

--- ship.py
+++ ship.py
@@ -1,7 +1,23 @@
+import pygame
+from pygame.locals import *
+
+
 class Ship(pygame.sprite.Sprite):
     """All of the logic for the players ship is contained
         within this class"""
 
-    def __init__(self):
-        pos = pygame.mouse.get_pos()
-        self.rect.midtop = pos
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.ship = pygame.image.load('Assets/spaceship.png').convert()
+        self.y = y
+        self.x = x
+
+    def draw(self, screen):
+        screen.blit(self.ship, (self.x, self.y))
+
+    def controls(self):
+        keys = pygame.key.get_pressed()
+        if keys[K_LEFT]:
+            self.x -= 5
+        if keys[K_RIGHT]:
+            self.x += 5
