ca9a638c68167ba66e74073e4056f1c623ea417a
Matthew Cairns <mattrcairns@gmail.com>
2013-11-17 21:14:33 +0000

    Changed variable names.

--- ship.py
+++ ship.py
@@ -8,33 +8,32 @@ class Ship(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.ship = pygame.image.load('Assets/spaceship.png').convert()
-        self.y = y
-        self.x = x
+        self.image = pygame.image.load('Assets/starship.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
 
-    def draw(self, screen):
-        screen.blit(self.ship, (self.x, self.y))
 
-    def controls(self, screen):
+    def controls(self):
         keys = pygame.key.get_pressed()
         if keys[K_LEFT]:
-            self.x -= 5
+            self.rect.x -= 5
         if keys[K_RIGHT]:
-            self.x += 5
+            self.rect.x += 5
 
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, screen):
         pygame.sprite.Sprite.__init__(self)
-        self.bullet = pygame.image.load('Assets/bullet.png').convert()
-        print 'BULLET'
-        self.x = x
-        self.y = y
+        self.image = pygame.image.load('Assets/bullet.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
     def draw(self, screen):
-        screen.blit(self.bullet, (self.x, self.y))
+        screen.blit(self.image, (self.rect.x, self.rect.y))
 
     def update(self):
-        self.y -= 10
+        self.rect.y -= 10
 
 
