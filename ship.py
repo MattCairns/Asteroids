ca677038596a4e91d95e43d1751a38d3d5ee14e7
Matthew Cairns <mattrcairns@gmail.com>
2013-11-20 22:44:24 +0000

    Added rotational movement to ship class.

--- ship.py
+++ ship.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import math
 
 
 class Ship(pygame.sprite.Sprite):
@@ -13,13 +14,49 @@ class Ship(pygame.sprite.Sprite):
         self.rect.y = y
         self.rect.x = x
 
+        self.vel_y = 0
+        self.vel_x = 0
+
+        self.velocity = 0.1
+        self.friction = 0.98
+        self.current_angle = 90.0
 
     def controls(self):
+        radians = (math.pi/180) * self.current_angle
         keys = pygame.key.get_pressed()
+        if keys[K_UP]:
+            self.vel_x += math.sin(radians) * self.velocity
+            self.vel_y += math.cos(radians) * self.velocity
         if keys[K_LEFT]:
-            self.rect.x -= 5
+            self.current_angle += 1
         if keys[K_RIGHT]:
-            self.rect.x += 5
+            self.current_angle -= 1
+
+    def check(self):
+        if self.rect.x < 0:
+            self.rect.x = 800
+        if self.rect.x > 800:
+            self.rect.x = 0
+        if self.rect.y < 0:
+            self.rect.y = 600
+        if self.rect.y > 600:
+            self.rect.y = 0
+        if self.current_angle < 0:
+            self.current_angle = 360
+        if self.current_angle > 360:
+            self.current_angle = 0
+
+    def update_angle(self, screen):
+        rot_image = pygame.transform.rotate(self.image, self.current_angle)
+        rot_rect = rot_image.get_rect(center=self.rect.center)
+        screen.blit(rot_image, (self.rect.x, self.rect.y))
+
+    def update(self):
+        self.vel_y *= self.friction
+        self.vel_x *= self.friction
+
+        self.rect.x -= self.vel_x
+        self.rect.y -= self.vel_y
 
 
 class Bullet(pygame.sprite.Sprite):
