910a3c790f6b014729e810ea08602454a4f41ef7
Matthew Cairns <mattrcairns@gmail.com>
2013-11-21 18:09:52 +0000

    -Bullets now shoot properly.

--- ship.py
+++ ship.py
@@ -14,10 +14,12 @@ class Ship(pygame.sprite.Sprite):
         self.rect.y = y
         self.rect.x = x
 
+        self.pxlen = 50
+
         self.vel_y = 0.0
         self.vel_x = 0.0
 
-        self.velocity = 0.1
+        self.velocity = 0.15
         self.friction = 0.98
         self.current_angle = 90.0
 
@@ -28,18 +30,18 @@ class Ship(pygame.sprite.Sprite):
             self.vel_x += math.sin(radians) * self.velocity
             self.vel_y += math.cos(radians) * self.velocity
         if keys[K_LEFT]:
-            self.current_angle += 1
+            self.current_angle += 2
         if keys[K_RIGHT]:
-            self.current_angle -= 1
+            self.current_angle -= 2
 
     def check(self):
-        if self.rect.x < 0:
+        if self.rect.x < 0 - self.pxlen:
             self.rect.x = 800
-        if self.rect.x > 800:
+        if self.rect.x > 800 + self.pxlen:
             self.rect.x = 0
-        if self.rect.y < 0:
+        if self.rect.y < 0 - self.pxlen:
             self.rect.y = 600
-        if self.rect.y > 600:
+        if self.rect.y > 600 + self.pxlen:
             self.rect.y = 0
         if self.current_angle < 0:
             self.current_angle = 360
@@ -48,8 +50,12 @@ class Ship(pygame.sprite.Sprite):
 
     def update_angle(self, screen):
         rot_image = pygame.transform.rotozoom(self.image, self.current_angle, 1)
-        rot_rect = rot_image.get_rect(center=self.rect.center)
+        rot_image.get_rect().center = rot_image.get_rect().center
+        rot_image.get_rect().center = (25, 25)
         screen.blit(rot_image, (self.rect.x, self.rect.y))
+
+    def get_ship_angle(self):
+        return self.current_angle
 
     def update(self):
         self.vel_y *= self.friction
@@ -60,17 +66,34 @@ class Ship(pygame.sprite.Sprite):
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y, screen):
+    def __init__(self, x, y, angle):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('Assets/bullet.png').convert_alpha()
+        self.image = pygame.image.load('Assets/projectile.png').convert_alpha()
         self.rect = self.image.get_rect()
+
+        self.bullet_angle = angle
+        self.radians = (math.pi/180) * self.bullet_angle
+
+
+
+        self.velocity = 10
+
+        self.vel_x = math.sin(self.radians) * self.velocity
+        self.vel_y = math.cos(self.radians) * self.velocity
+
         self.rect.x = x
         self.rect.y = y
 
-    def draw(self, screen):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
-
     def update(self):
-        self.rect.y -= 10
+
+        self.rect.y -= self.vel_y
+        self.rect.x -= self.vel_x
+
+    def draw(self, screen):
+        rot_image = pygame.transform.rotozoom(self.image, self.bullet_angle, 1)
+        rot_image.get_rect().center = rot_image.get_rect().center
+        screen.blit(rot_image, (self.rect.x + 25, self.rect.y + 25))
+
+
 
 
