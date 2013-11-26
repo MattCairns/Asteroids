c1c6844218440c476bc81bee39ec13d2a0a5eaf7
Matthew Cairns <mattrcairns@gmail.com>
2013-11-26 19:50:45 +0000

    Added gameover screen

--- helper.py
+++ helper.py
@@ -2,7 +2,7 @@ import pygame
 
 
 
-def game_over(screen):
+def game_over(screen, game_over):
     font = pygame.font.SysFont('Helvetica', 30)
     fps_label = font.render('GAME OVER', 1, (255, 0, 0))
     screen.blit(fps_label, (400,300))
