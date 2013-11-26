47528cfb307ee69bce5b090c9b99201e312c540f
Matthew Cairns <mattrcairns@gmail.com>
2013-11-26 19:50:20 +0000

    Added scoring and fps

--- helper.py
+++ helper.py
@@ -1,7 +1,10 @@
 import pygame
 
 
+
 def game_over(screen):
     font = pygame.font.SysFont('Helvetica', 30)
     fps_label = font.render('GAME OVER', 1, (255, 0, 0))
     screen.blit(fps_label, (400,300))
+
+
